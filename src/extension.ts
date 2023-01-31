25290d50794fd3d1affe0756a1bc536bfd4ec688
Karol Harezlak <76980800+karolh2000@users.noreply.github.com>
2023-01-30 16:50:16 -0800

    Connect welcome view (UI) and new connection status bar (#24)
    * New UI/functionality
     - Welcome view added.
     - Status bar added
     - NodeConfig view removed and the functionality was moved to the status bar.
    
    * Update package.json - removing redundant condition.
    Co-authored-by: Xuefei Han <xuefeih@jfrog.com>
    
    * The welcome view messages updated.
    Co-authored-by: Manas Kumar Das <das.manas@gmail.com>
    
    ---------
    
    Co-authored-by: Xuefei Han <xuefeih@jfrog.com>
    Co-authored-by: Manas Kumar Das <das.manas@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,13 @@
 import * as vscode from "vscode";
-import { NodeConfigView } from "./views/NodeConfigView";
-// import { NodeStatusViewProvider } from "./webviews/NodeStatusView";
 import { IntegrationsView as IntegrationsView } from "./views/IntegrationsView";
 import { Util } from "./utilities/Util";
 import { HelpView } from "./views/HelpView";
 import { Integration } from "./api/Integration";
 import { DockerIntegration } from "./integrations/DockerIntegration";
+import { ConnectionStatusBar } from "./views/ConnectionStatusBar";
+
+// This const is used for how often we should check if the Pyrsia node is online
+const REFRESH_UI_INTERVAL = 60 * 1000;
 
 export const activate = (context: vscode.ExtensionContext) => {
 	// Init the extension utils
@@ -14,16 +16,24 @@ export const activate = (context: vscode.ExtensionContext) => {
 	// Create docker integration
 	const dockerIntegration: Integration = new DockerIntegration(context);
 
-	// Create the node config view
-	const nodeConfigView = new NodeConfigView(context);
-	nodeConfigView.addIntegration(dockerIntegration);
-
 	// Create the integrations view
 	const integrationView = new IntegrationsView(context);
 	integrationView.addIntegration(dockerIntegration);
 
 	// Create Help view
 	new HelpView(context);
+
+	// Create Status Bar
+	const connectionStatusBar = new ConnectionStatusBar(context);
+
+	// trigger the UI updates every 10 seconds based on the
+	setInterval(() => {
+		// update the status bar
+		connectionStatusBar.requestUpdateStatusBar();
+		// update the integrations
+		IntegrationsView.requestIntegrationsUpdate();
+		IntegrationsView.requestIntegrationsViewUpdate();
+	}, REFRESH_UI_INTERVAL);
 };
 
 export const deactivate = () => {
